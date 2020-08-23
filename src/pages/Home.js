50e324e24b7a5dfc35e87584456503c19b1dfc94
MasonWang025 <mason.wang0025@gmail.com>
2020-08-23 10:38:41 -0700

    temporary home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Hero from "../components/Home/Hero";
 
 export default function Home() {
-  return <div className="home page">Homepage</div>;
+  return (
+    <div className="home page">
+      <Hero />
+    </div>
+  );
 }
