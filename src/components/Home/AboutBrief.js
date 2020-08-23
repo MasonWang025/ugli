e5dba7cfa991729d57a69f3754eae6b6b834ce7a
MasonWang025 <mason.wang0025@gmail.com>
2020-08-23 10:59:09 -0700

    section style base

---
+++ src/components/Home/AboutBrief.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function AboutBrief() {
+  return (
+    <div className="about-brief section">
+      <div className="container">
+        <div className="row">
+          <div className="col-8">
+            <h1 className="section-heading">
+              <span className="underline--magical">
+                162,000,
+                <br className="d-sm-none d-block" />
+                000,000
+              </span>
+            </h1>
+          </div>
+          <div className="col-4">s</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Home/Hero";
+import AboutBrief from "../components/Home/AboutBrief";
 
 export default function Home() {
   return (
     <div className="home page">
       <Hero />
+      <AboutBrief />
     </div>
   );
 }
