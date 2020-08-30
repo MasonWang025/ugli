db08c3fed9b6789414090fa3f00864e2ba364e5e
rvignav <rvignav@gmail.com>
2020-08-30 00:01:10 -0700

    Add material-ui/core form

--- src/pages/Delivery.js
+++ src/pages/Delivery.js
@@ -1,25 +1,62 @@
 import React from "react";
-import Hero from "../components/Home/Hero";
-import { FForm, Frame } from 'react-fullscreen-form';
+import ReactDOM from "react-dom";
+import Select from "react-select";
+import { useForm, Controller } from "react-hook-form";
+import { Checkbox, Input } from "@material-ui/core";
+import { Input as AntdInput } from "antd";
+
+import "../styles/pages/delivery.scss";
 
 
 export default function Delivery() {
+  const { control, handleSubmit } = useForm();
+
+  const onSubmit = data => {
+    alert(JSON.stringify(data));
+  };
+
   return (
-    <div className="delivery page">
-      <FForm title="" style={{top: 100}}>
-    <Frame title="What's your name?">
-      <input type="string" required />
-    </Frame>
-    <Frame title="What's your email address?">
-      <input type="email" placeholder="mtkadayifci@gmail.com" required />
-    </Frame>
-    <Frame title="What's your phone number?">
-      <input type="phone" required />
-    </Frame>
-    <Frame title="How old are you?">
-      <input type="number" required />
-    </Frame>
-  </FForm>
-  </div>
+    <div><p>helloi</p></div>
+    // <form onSubmit={handleSubmit(onSubmit)}>
+    //   <label>First Name</label>
+    //   <Controller
+    //     as={Input}
+    //     name="firstName"
+    //     control={control}
+    //     defaultValue=""
+    //     className="materialUIInput"
+    //   />
+    //   <label>First Name</label>
+    //   <Controller
+    //     as={AntdInput}
+    //     name="lastName"
+    //     control={control}
+    //     defaultValue=""
+    //   />
+    //   <label>Ice Cream Preference</label>
+    //   <Controller
+    //     name="iceCreamType"
+    //     as={Select}
+    //     options={[
+    //       { value: "chocolate", label: "Chocolate" },
+    //       { value: "strawberry", label: "Strawberry" },
+    //       { value: "vanilla", label: "Vanilla" }
+    //     ]}
+    //     control={control}
+    //     defaultValue=""
+    //   />
+    //   <Controller
+    //     name="Checkbox"
+    //     control={control}
+    //     render={props => (
+    //       <Checkbox
+    //         onChange={e => props.onChange(e.target.checked)}
+    //         checked={props.value}
+    //       />
+    //     )}
+    //   />
+
+    //   <input type="submit" />
+    // </form>
   );
 }
